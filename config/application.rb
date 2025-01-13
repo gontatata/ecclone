c5a51eab20bdc74253ddb060a0fa541b89692fd9
EC2 Default User <ec2-user@ip-172-31-22-234.ap-northeast-1.compute.internal>
2025-01-13 07:01:18 +0000

    tailwindの導入

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,19 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.helper false
+      g.test_framework  :test_unit, fixture: false
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Tokyo"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
